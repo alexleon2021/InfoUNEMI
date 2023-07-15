f1531cccfeb90785a75271bc28a7ec1d6b69eb16
Esaú Gamboa <esagambo@hotmail.com>
2023-07-14 22:47:49 -0500

    Añadido de Dave Estructura de Archivos

--- administrador/urls.py
+++ administrador/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [ ]
+urlpatterns = [
+    path('login', views.login, name='login'),
+    path('reestablecer', views.recuperarC, name='recuperarC'),
+ ]
