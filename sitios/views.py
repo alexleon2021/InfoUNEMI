263ae27ed4d72ede7c1e349beb15487b6747ee93
CarolinaMoran03 <carolinacsm14@gmail.com>
2023-07-29 03:37:25 -0500

    clasificacion_nivel

--- sitios/views.py
+++ sitios/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Bloque
-
 # Create your views here.
 
 def home(request):
@@ -10,8 +9,9 @@ def home(request):
 def bloque(request, bloque_id):
     bloque = get_object_or_404(Bloque, id=bloque_id)
     sitios = bloque.sitio_set.all()
-    return render(request, 'bloque.html', {'bloque': bloque, 'sitios': sitios})
+    return render(request, 'bloque.html', {'bloque': bloque, 'sitios': sitios })
 
 def listadoBloques(request):
     bloques = Bloque.objects.all()
     return render(request, 'listadoBloques.html', {'bloques': bloques})
+
